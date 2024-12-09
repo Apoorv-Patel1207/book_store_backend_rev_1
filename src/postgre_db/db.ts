2be014b8522e72fd42d29bbe6b2e689156a8854b
Apoorv Patel <apoorv.patel@CELSYSWTC.IN>
2024-12-09 18:49:53 +0530

    created the docker file

--- src/postgre_db/db.ts
+++ src/postgre_db/db.ts
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "bookstore_db",
-  password: "password",
-  port: 5432,
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST || "localhost",
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
 });
 
 module.exports = pool;
